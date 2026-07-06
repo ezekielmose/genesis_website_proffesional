fcda924c4e242bb5d69dfd37f752e9262652ca58
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2026-07-06 11:38:21 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/layout/Navbar";
-import Hero from "@/components/home/Hero/hero";
+import Hero from "@/components/home/Hero/Hero";
 import StatsSection from "@/components/home/Stats/StatsSection";
 import Solutions from "@/components/home/Solutions/Solutions";
 import Features from "@/components/home/Features/Features";
