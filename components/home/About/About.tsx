adc421d9088ee8c4e485ec70eb023eb68cf7966f
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2026-07-06 12:59:36 +0300

    Update About.tsx

--- components/home/About/About.tsx
+++ components/home/About/About.tsx
@@ -150,7 +150,7 @@ export default function About() {
 
                 <div>
 
-                  <h3 className="text-2xl font-bold">
+                  <h3 className="text-2xl font-bold text-slate-900">
 
                     Our Mission
 
@@ -182,7 +182,7 @@ export default function About() {
 
                 <div>
 
-                  <h3 className="text-2xl font-bold">
+                  <h3 className="text-2xl font-bold text-slate-900">
 
                     Our Vision
 
